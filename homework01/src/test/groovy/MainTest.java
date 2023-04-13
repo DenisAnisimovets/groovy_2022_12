0a1e233cb6abdc1962a72d5621d3ae0e263c420a
Денис <anisimovets@mail.ru>
2023-04-13 22:27:43 +0300

    hw01 init

---
+++ homework01/src/test/groovy/MainTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Тест просто вызывается")
+class MainTest {
+
+    @Test
+    void main() {
+        assert 1==1;
+    }
+}
