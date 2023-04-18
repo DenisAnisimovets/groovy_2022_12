6752fb213ea80c6e448d48e51e91eb2ae2c79256
Денис <anisimovets@mail.ru>
2023-04-18 18:33:47 +0300

    hw03

---
+++ homework03/src/main/groovy/com/danis/model/Action.groovy
@@ -0,0 +1,20 @@
+package com.danis.model
+
+import java.time.LocalTime
+
+class Action {
+    int actionId
+    String action
+    LocalTime duration
+
+    Action(int actionId, String action, LocalTime duration) {
+        this.actionId = actionId
+        this.action = action
+        this.duration = duration
+    }
+
+    Action(String action, LocalTime duration) {
+        this.action = action
+        this.duration = duration
+    }
+}
---
+++ homework03/src/main/groovy/com/danis/repo/Repo.groovy
@@ -0,0 +1,38 @@
+package com.danis.repo
+
+import com.danis.model.Task
+
+@Singleton
+class Repo {
+    List<Task> tasks = new ArrayList<>()
+
+    def addTask(Task newTask) {
+        def max = tasks.max {it.taskId}
+        newTask.with {
+            taskId = max.taskId + 1
+        }
+        tasks << newTask
+    }
+
+    def getTask(int id) {
+        tasks.find {it.taskId == id}
+    }
+
+    def modifyTask(Task task) {
+        def find = getTask(task.taskId)
+        deleteTask(find)
+        addTask(task)
+    }
+
+    def deleteTask(Task deleteTask) {
+        tasks.remove(deleteTask)
+    }
+
+    def getCountTasks() {
+        tasks.size()
+    }
+
+    def getListTasks() {
+        tasks
+    }
+}
